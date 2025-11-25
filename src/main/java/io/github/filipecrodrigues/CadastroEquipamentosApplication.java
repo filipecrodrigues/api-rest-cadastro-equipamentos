550928ff0e11b70a5759b5a18fe8beef2ee7ffd3
Filipe <filipe.candido.rodrigues@gmail.com>
2025-11-25 12:55:56 -0300

    Ajuste arquivos do projeto Spring Boot

---
+++ src/main/java/io/github/filipecrodrigues/CadastroEquipamentosApplication.java
@@ -0,0 +1,13 @@
+package io.github.filipecrodrigues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEquipamentosApplication.class, args);
+	}
+
+}
